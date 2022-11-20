72772fc41736ed1f50f8e29564ffeb5e8ca396d1
Tomoki <tomoki_ota@outlook.com>
2022-11-20 19:50:13 +0900

    initial commit with completed memory game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
+
+const CARD_IMG = [
+    { src: require("./assets/img/dog.png"), matched: false },
+    { src: require("./assets/img/cat.png"), matched: false },
+    { src: require("./assets/img/bird.png"), matched: false },
+    { src: require("./assets/img/dinosaur.png"), matched: false },
+    { src: require("./assets/img/dolphin.png"), matched: false },
+    { src: require("./assets/img/penguin.png"), matched: false },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cards, setCards] = useState([]);
+    const [turns, setTurns] = useState(0);
+    const [choiceOne, setChoiceOne] = useState(null);
+    const [choiceTwo, setChoiceTwo] = useState(null);
+    const [disabled, setDisabled] = useState(false);
+
+    // Shuffle Cards
+    const shuffleCards = () => {
+        const shuffledCards = [...CARD_IMG, ...CARD_IMG]
+            .sort(() => Math.random() - 0.5)
+            .map((card) => ({ ...card, id: Math.random() }));
+
+        setChoiceOne(null);
+        setChoiceTwo(null);
+        setCards(shuffledCards);
+        setTurns(0);
+    };
+
+    // Handle Choice
+    const handleChoice = (card) => {
+        choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    };
+
+    // compare 2 selected cards
+    useEffect(() => {
+        if (choiceOne && choiceTwo) {
+            setDisabled(true);
+            if (choiceOne.src === choiceTwo.src) {
+                setCards((prevCards) => {
+                    return prevCards.map((card) => {
+                        if (card.src === choiceOne.src) {
+                            return { ...card, matched: true };
+                        } else {
+                            return card;
+                        }
+                    });
+                });
+                resetTurn();
+            } else {
+                setTimeout(() => {
+                    resetTurn();
+                }, 1000);
+            }
+        }
+    }, [choiceOne, choiceTwo]);
+
+    // reset choices & increase turn
+    const resetTurn = () => {
+        setChoiceOne(null);
+        setChoiceTwo(null);
+        setTurns((prevTurns) => prevTurns + 1);
+        setDisabled(false);
+    };
+
+    // start a new game automatically
+    useEffect(() => {
+        shuffleCards();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>かーど あわせ げーむ</h1>
+            <button onClick={shuffleCards}>はじめから</button>
+
+            <div className="card-grid">
+                {cards.map((card) => (
+                    <SingleCard
+                        key={card.id}
+                        card={card}
+                        handleChoice={handleChoice}
+                        flipped={
+                            card === choiceOne ||
+                            card === choiceTwo ||
+                            card.matched
+                        }
+                        disabled={disabled}
+                    />
+                ))}
+            </div>
+            <p>えらんだ かいすう: {turns}かい</p>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/SingleCard.js
@@ -0,0 +1,25 @@
+import "./SingleCard.css";
+
+const SingleCard = ({ card, handleChoice, flipped, disabled, complete }) => {
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card);
+        }
+    };
+
+    return (
+        <div className="card">
+            <div className={flipped ? "flipped" : ""}>
+                <img className="front" src={card.src} alt="Card front" />
+                <img
+                    className="back"
+                    onClick={handleClick}
+                    src={require("../assets/img/card-back.png")}
+                    alt="Card back"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default SingleCard;
